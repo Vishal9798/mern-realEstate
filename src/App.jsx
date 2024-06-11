3023f06d072dd85e55ac25d2eb3311899e852f59
Vishal9798 <vishal.p12344@gmail.com>
2024-06-11 21:58:15 +0530

    create header componenets

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./componenets/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
