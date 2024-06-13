e3ba60511d570164f05156647619674266258f0e
Vishal9798 <vishal.p12344@gmail.com>
2024-06-13 13:06:46 +0530

    created a middleware and function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'; //install bcryptjs package
-
-export const signup = async (req, res) => {
+import { errorHandler } from '../utils/error.js';
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); // for encrypting password in db
     const newUser = new User({ username, email, password:hashedPassword });
@@ -12,7 +12,8 @@ export const signup = async (req, res) => {
       await newUser.save();
       res.status(201).json('user created');
     } catch(error){
-       res.status(500).json(error.message);   
+      // next(errorHandler(440,'error created by me')); custom error by me
+      next(error);
     }
    
   };
--- api/index.js
+++ api/index.js
@@ -24,3 +24,14 @@ app.listen(3000, () => {
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+
+//creating middleware for error handling
+app.use((err,req,res,next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success:false,
+    statusCode:statusCode,
+    message:message,
+  });
+});
