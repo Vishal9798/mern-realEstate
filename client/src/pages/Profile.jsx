b0b5af628d6b7cc6e5f3ca5c6cbb17315169f3f6
Vishal9798 <vishal.p12344@gmail.com>
2024-07-04 21:36:24 +0530

    Completed ShowListings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,6 +27,8 @@ function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false)
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]); // [state , setState]
   const dispatch = useDispatch();
   //console.log(formData);
   // console.log(filePerc)
@@ -138,6 +140,22 @@ function Profile() {
     }
   }
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-bold text-center my-5'>PROFILE</h1>
@@ -163,9 +181,9 @@ function Profile() {
         <input onChange={handleChange} id='email' type='email' placeholder='email' className='border p-2 rounded-lg' defaultValue={currentUser.email} />
         <input onChange={handleChange} id='password' type='password' placeholder='password' className='border p-2 rounded-lg' />
         <button disabled={loading} className='bg-green-500 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70'>{loading ? 'Loading...' : 'Update'}</button>
-     <Link className='bg-green-500 text-white p-3 rounded-lg uppercase text-center hover:opacity-90 disabled:opacity-70' to={"/create-listing"}>Create Listing
-     </Link>
-     
+        <Link className='bg-green-500 text-white p-3 rounded-lg uppercase text-center hover:opacity-90 disabled:opacity-70' to={"/create-listing"}>Create Listing
+        </Link>
+
       </form>
       <div className='flex justify-between mt-2'>
         <span onClick={handleDeleteUser} className='text-red-500 cursor-pointer'>Delete Account</span>
@@ -173,8 +191,39 @@ function Profile() {
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated Successfully!' : ''}</p>
+      <button onClick={handleShowListings} className='text-green-700 w-full' >Show Listings</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing Listings' : ''}</p>
+
+      {userListings && userListings.length > 0 &&
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-bold'>
+          Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div key={listing._id} className='gap-3 border rounded-lg p-3 flex justify-between items-center'>
+              <Link to={`/listing/${listing._id}`} className='text-green-700'>
+                <img className='h-16 w-16 object-contain' src={listing.imageUrls[0]} alt='listing cover' />
+              </Link>
+
+              <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`} >
+                <p >
+                  {listing.name}
+                </p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button className='text-red-700 uppercase' >Delete</button>
+                <button className='text-green-700 uppercase' >Edit</button>
+              </div>
+
+
+            </div>
+          ))}
+        </div>}
+
+
     </div>
-  )
+  );
 }
 
 export default Profile
