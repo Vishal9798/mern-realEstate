b306e2c0cde266c7e5c36d8d4205b7ce97f7493b
Vishal9798 <vishal.p12344@gmail.com>
2024-07-15 14:55:15 +0530

    fixed some deployment changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -13,6 +12,9 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      external: ['bcryptjs', 'express', 'mongoose', 'jsonwebtoken', 'cookie-parser', 'dotenv']
+    }
   },
   plugins: [react()],
 })
