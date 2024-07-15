6cc737f78814295f3ded65f503e5b1e588448910
Vishal9798 <vishal.p12344@gmail.com>
2024-07-15 13:33:40 +0530

    Created About Page

--- client/src/componenets/Header.jsx
+++ client/src/componenets/Header.jsx
@@ -51,7 +51,7 @@ function Header() {
 
         </form>
         <ul className='flex gap-4'>
-          <Link to='/home'>
+          <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
           </Link>
           <Link to='/about'>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-function About() {
+export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+  <h1 className='text-3xl font-bold mb-4 text-slate-800'>About HomeNest</h1>
+  <p className='mb-4 text-slate-700'>
+    HomeNest is a premier real estate agency renowned for our expertise in assisting clients with buying, selling, and renting properties in the most sought-after neighborhoods. Our team of seasoned agents is committed to delivering exceptional service, ensuring that the process of buying or selling a property is seamless and stress-free.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    Our mission is to empower our clients to achieve their real estate aspirations through expert guidance, personalized service, and a profound understanding of the local market. Whether you are purchasing, selling, or renting, HomeNest is here to support you at every stage of your real estate journey.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    Our team comprises highly experienced and knowledgeable agents dedicated to providing the utmost level of service to our clients. We believe that the experience of buying or selling a property should be both exciting and rewarding, and we are passionate about making this a reality for each of our clients.
+  </p>
+  <p className='mb-4 text-slate-700'>
+    At HomeNest, we pride ourselves on our integrity, professionalism, and commitment to excellence. Our comprehensive services and personalized approach ensure that we meet the unique needs of every client, guiding them through each step of the real estate process with confidence and ease.
+  </p>
+  <p className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-4 text-gray-500 text-sm">
+    Developed by @VishalKumar
+  </p>
+</div>
+
   )
 }
-
-export default About
